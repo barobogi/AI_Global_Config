e7a597de5e6e5e9a64a9789d2f80e0bb54e109b8
barobogi <barobogi79@gmail.com>
2026-07-12 10:05:45 +0900

    auto: Cowork/CLI 동기화 2026-07-12 10:05

--- AI_hub/shared/T020_human_in_the_loop/start_bot.bat
+++ AI_hub/shared/T020_human_in_the_loop/start_bot.bat
@@ -2,7 +2,7 @@
 chcp 65001 >nul
 cd /d %~dp0
 
-for /f "tokens=2 delims== " %%i in ('findstr "APPROVAL_BOT_TOKEN" D:\Dev\n8n_start.bat') do set APPROVAL_BOT_TOKEN=%%i
+for /f "tokens=2 delims==" %%i in ('findstr "APPROVAL_BOT_TOKEN" D:\Dev\n8n_start.bat') do set APPROVAL_BOT_TOKEN=%%i
 
 if "%APPROVAL_BOT_TOKEN%"=="" (
     echo [ERROR] APPROVAL_BOT_TOKEN을 D:\Dev\n8n_start.bat에서 찾을 수 없습니다.
