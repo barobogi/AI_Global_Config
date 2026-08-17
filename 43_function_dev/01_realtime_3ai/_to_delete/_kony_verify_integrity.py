577e8174184cfbdde9ea9fe0c1fb4094477e8c49
barobogi <barobogi79@gmail.com>
2026-08-17 21:00:11 +0900

    auto: Cowork/CLI 동기화 2026-08-17 21:00

---
+++ 43_function_dev/01_realtime_3ai/_to_delete/_kony_verify_integrity.py
@@ -0,0 +1,15 @@
+import sys, sqlite3
+sys.path.insert(0, '.')
+from realtime_engine import Realtime3AIEngine
+
+def _patched_get_connection(self):
+    conn = sqlite3.connect(str(self.db_path), timeout=10.0)
+    conn.row_factory = sqlite3.Row
+    conn.execute('PRAGMA locking_mode = EXCLUSIVE;')
+    conn.execute('PRAGMA busy_timeout = 8000;')
+    return conn
+Realtime3AIEngine._get_connection = _patched_get_connection
+
+engine = Realtime3AIEngine()
+result = engine.verify_log_integrity()
+print(result)
