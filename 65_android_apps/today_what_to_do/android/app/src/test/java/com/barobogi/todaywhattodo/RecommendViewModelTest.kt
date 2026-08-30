dcbbeec172c50612f9a6f4114e44abd875104740
barobogi <barobogi79@gmail.com>
2026-08-30 23:09:57 +0900

    auto: Cowork/CLI 동기화 2026-08-30 23:09

--- 65_android_apps/today_what_to_do/android/app/src/test/java/com/barobogi/todaywhattodo/RecommendViewModelTest.kt
+++ 65_android_apps/today_what_to_do/android/app/src/test/java/com/barobogi/todaywhattodo/RecommendViewModelTest.kt
@@ -15,7 +15,7 @@ class RecommendViewModelTest {
     fun testHaversineKmAccuracy() {
         // 서울 종로 (37.5796, 126.9770) -> 부산 해운대 (35.1587, 129.1604)
         val distSeoulToBusan = viewModel.haversineKm(37.5796, 126.9770, 35.1587, 129.1604)
-        assertTrue("서울-부산 거리는 약 320km~330km 사이여야 함", distSeoulToBusan in 320.0..330.0)
+        assertTrue("서울-부산 거리는 약 320km~340km 사이여야 함", distSeoulToBusan in 320.0..340.0)
 
         // 인천 송도 (37.3925, 126.6394) -> 서울 경복궁 (37.5796, 126.9770)
         val distIncheonToSeoul = viewModel.haversineKm(37.3925, 126.6394, 37.5796, 126.9770)
